cac2e1a87d4453145b0effb9f6635b5a3fc7c1c5
k-p5w <kos.p5w@gmail.com>
2020-02-20 17:44:44 +0900

    ＋記号を付ける対応

--- extension.js
+++ extension.js
@@ -13,75 +13,101 @@ function activate(context) {
     console.log('welcome to mindmap.');
 
     // mindmapincrement is カーソル位置が先頭であれば、先頭文字を確認し、[+|-|*]以外であれば[+]を追加する。
-    let disposable = vscode.commands.registerCommand('extension.mindmapincrement', function() {
+    let disposable = vscode.commands.registerCommand('extension.mindmap.increment', function() {
         // 以下エントリを参考に実装してみる
         // https://qiita.com/delmontz/items/abd075eacb1302fc184c
 
         if (!vscode.window.activeTextEditor) {
             return; // no editor
         }
-
-        // ここに実装コードを書く
-        const doc = vscode.window.activeTextEditor;
+        var newtext = "";
+        // アクティブなエディタを取得
+        const editor = vscode.window.activeTextEditor;
 
         //選択範囲を取得
-        const ref = doc.selection;
+        const ref = editor.selection;
 
-        if (ref) {
-            // 選択範囲なし            
-
-
+        // 選択範囲がされていない場合
+        if (editor.selection.isEmpty) {
+            console.log('範囲選択なし.');
+            // var startPos = editor.document.positionAt(0);
+            // var endPos = editor.document.positionAt(editor.document.getText().length - 1);
+            // var range = new vscode.Range(startPos, endPos);
             var IsPos0 = false;
-
-            // todo:先頭の文字列を取得する。。
-
             // 1-1.カーソル位置が先頭の場合
             if (IsPos0) {
-                var IsKeyWord = false;
-
-
-                // todo:1文字目から分のはじまりまでを取得する。。
-
-                // 1-1-1.開始文字が plantumlで使う予約語？（[+|-|*]）の場合
-                if (IsKeyWord) {
-                    // 1-1-1-a.+ の場合			
-                    var IsMapLeft = false;
-                    var IsMapRight = false;
-
-                    // todo:1文字目だけを取得する。。
-
-                    if (IsMapLeft) {
-                        // - 開始文字の前に、[+]を追加する
-                        return [vscode.TextEdit.insert(firstLine.range.start, '+')];
-                    } else if (IsMapRight) {
-                        // 1-1-1-b.- の場合
-                        // - マイナスを1つ消す。
-                    } else {
-                        // 1-1-1-c.* の場青
-                        // - なにもしない
-                    }
-
-                } else {
-                    // 1-1-2.上記以外の場合
-                    // - 開始文字の前に、[+ ]を追加する
-                    return [vscode.TextEdit.insert(firstLine.range.start, '+')];
-                }
-
-
 
             } else {
                 // 1-2.上記以外
-                // - この拡張としてはなにもしない
+                // 選択されていれば選択範囲の先頭文字に対して同様の処理を行う
 
             }
 
-
-
-
-
-
         } else {
+
             // 選択範囲あり
+
+            // 選択された文字列を取得する
+            const selectedtxt = editor.document.getText(ref);
+            newtext = selectedtxt;
+            console.log(selectedtxt);
+            var srcword = selectedtxt.substring(0, 1);
+
+            var IsKeyWord = false;
+
+            const regex = RegExp(`[\\+|\\-|\*|']`);
+            console.log("今回の選択文字：", srcword);
+            // 予約語ではじまっているか？
+            IsKeyWord = regex.test(srcword);
+
+            // todo:1文字目から分のはじまりまでを取得する。。
+
+            // 1-1-1.開始文字が plantumlで使う予約語？（[+|-|*|']）の場合
+            if (IsKeyWord) {
+                console.log('予約語でした.');
+
+                // チェック用文字列の準備
+                const regexplus = RegExp("\\+");
+                const regminus = RegExp("\\-");
+                var IsMapLeft = false;
+
+                IsMapLeft = regexplus.test(srcword);
+
+                var IsMapRight = false;
+                IsMapRight = regminus.test(srcword);
+                console.log(IsMapLeft);
+                console.log(IsMapRight);
+                if (IsMapLeft) {
+                    // 1-1-1-a.+ の場合		
+
+                    // + 文字であればスペースなしで+だけを増やす
+                    console.log('予約語＋でした.');
+                    // - 開始文字の前に、[+]を追加する
+                    newtext = "+" + selectedtxt;
+
+                } else if (IsMapRight) {
+                    console.log('予約語－でした.');
+                    // 1-1-1-b.- の場合
+                    // - マイナスを1つ消す。
+                    var endpos = selectedtxt.length;
+                    newtext = selectedtxt.substring(1, endpos);
+
+                } else {
+                    console.log('予約語＠その他でした.');
+                    // 1-1-1-c.* の場青
+                    // - なにもしない
+                }
+
+            } else {
+                // 1-1-2.上記以外の場合
+                console.log('予約語以外でした.');
+                newtext = "+ " + selectedtxt;
+            }
+            // texteditor側に反映させる
+            editor.edit(builder => {
+                builder.replace(ref, newtext)
+            })
+            return [];
         }
 
     });
