cfa12a424ce97d30dd6feba35c1ae5b99ee9df78
k-p5w <kos.p5w@gmail.com>
2020-02-25 15:29:01 +0900

    decrement対応と説明の編集

--- README.md
+++ README.md
@@ -1,20 +1,18 @@
-# puml-mind README
+# mindmap-diagram-supporter README
 
-This is the README for your extension "puml-mind". After writing up a brief description, we recommend including the following sections.
+plantumlでマインドマップを書くときに便利になるためのツール（VSCODE拡張）です。
+　 > https://plantuml.com/ja/mindmap-diagram
 
-## Features
+## Features / どんなことができる
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+テキストエディタ上で、文字が選択されていた場合に、[+]を追加する
+  > mindmapincrement
+テキストエディタ上で、文字が選択されていた場合に、[-]を追加する
+  > mindmapdecrement
 
-For example if there is an image subfolder under your extension project workspace:
+## Requirements /　使うために必要なもの
 
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
++キーと-キーを追加するだけなので、無くても動きますが、vscodeのplantumlの拡張が入っていることを前提で作っています。
 
 ## Extension Settings
 
@@ -27,39 +25,14 @@ This extension contributes the following settings:
 * `myExtension.enable`: enable/disable this extension
 * `myExtension.thing`: set to `blah` to do something
 
-## Known Issues
+## Known Issues / 既知の課題や問題点
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
+改行コードの複数対応は未確認です。
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
+2020/02/22:それなりに動くものになる
 
 ### 1.0.0
 
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+初回リリース。
--- extension.js
+++ extension.js
@@ -1,9 +1,7 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// キーバインド想定：
+// editorTextFocus && !editorReadonly && !editorTabMovesFocus && editorLangId == 'diagram'
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -11,112 +9,213 @@ const vscode = require('vscode');
 function activate(context) {
 
     console.log('welcome to mindmap.');
-
-    // mindmapincrement is カーソル位置が先頭であれば、先頭文字を確認し、[+|-|*]以外であれば[+]を追加する。
-    let disposable = vscode.commands.registerCommand('extension.mindmap.increment', function() {
-        // 以下エントリを参考に実装してみる
-        // https://qiita.com/delmontz/items/abd075eacb1302fc184c
-
-        if (!vscode.window.activeTextEditor) {
-            return; // no editor
-        }
-        var newtext = "";
-        // アクティブなエディタを取得
-        const editor = vscode.window.activeTextEditor;
-
-        //選択範囲を取得
-        const ref = editor.selection;
-
-        // 選択範囲がされていない場合
-        if (editor.selection.isEmpty) {
-            console.log('範囲選択なし.');
-            // var startPos = editor.document.positionAt(0);
-            // var endPos = editor.document.positionAt(editor.document.getText().length - 1);
-            // var range = new vscode.Range(startPos, endPos);
-            var IsPos0 = false;
-            // 1-1.カーソル位置が先頭の場合
-            if (IsPos0) {
-
-            } else {
-                // 1-2.上記以外
-                // 選択されていれば選択範囲の先頭文字に対して同様の処理を行う
-
-            }
-
-        } else {
-
-            // 選択範囲あり
-
-            // 選択された文字列を取得する
-            const selectedtxt = editor.document.getText(ref);
-            newtext = selectedtxt;
-            console.log(selectedtxt);
-            var srcword = selectedtxt.substring(0, 1);
-
-            var IsKeyWord = false;
-
-            const regex = RegExp(`[\\+|\\-|\*|']`);
-            console.log("今回の選択文字：", srcword);
-            // 予約語ではじまっているか？
-            IsKeyWord = regex.test(srcword);
-
-            // todo:1文字目から分のはじまりまでを取得する。。
-
-            // 1-1-1.開始文字が plantumlで使う予約語？（[+|-|*|']）の場合
-            if (IsKeyWord) {
-                console.log('予約語でした.');
-
-                // チェック用文字列の準備
-                const regexplus = RegExp("\\+");
-                const regminus = RegExp("\\-");
-                var IsMapLeft = false;
-
-                IsMapLeft = regexplus.test(srcword);
-
-                var IsMapRight = false;
-                IsMapRight = regminus.test(srcword);
-                console.log(IsMapLeft);
-                console.log(IsMapRight);
-                if (IsMapLeft) {
-                    // 1-1-1-a.+ の場合		
-
-                    // + 文字であればスペースなしで+だけを増やす
-                    console.log('予約語＋でした.');
-                    // - 開始文字の前に、[+]を追加する
-                    newtext = "+" + selectedtxt;
-
-                } else if (IsMapRight) {
-                    console.log('予約語－でした.');
-                    // 1-1-1-b.- の場合
-                    // - マイナスを1つ消す。
-                    var endpos = selectedtxt.length;
-                    newtext = selectedtxt.substring(1, endpos);
-
-                } else {
-                    console.log('予約語＠その他でした.');
-                    // 1-1-1-c.* の場青
-                    // - なにもしない
-                }
-
-            } else {
-                // 1-1-2.上記以外の場合
-                console.log('予約語以外でした.');
-                newtext = "+ " + selectedtxt;
-            }
-            // texteditor側に反映させる
-            editor.edit(builder => {
-                builder.replace(ref, newtext)
-            })
-            return [];
-        }
-
-    });
-
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(vscode.commands.registerCommand('extension.mindmapincrement', mindmapincrement));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.mindmapdecrement', mindmapdecrement));
 }
 exports.activate = activate;
 
+// IsTagert is 処理対象かチェックする
+function IsTagert() {
+    // editorじゃないとき
+    if (!vscode.window.activeTextEditor) {
+        return false; // no editor
+    }
 
+    // アクティブなエディタを取得
+    const editor = vscode.window.activeTextEditor;
+
+    // 選択範囲がされていない場合
+    if (editor.selection.isEmpty) {
+        // なにもしない
+        console.log('範囲選択なし.');
+        return false;
+
+    } else {
+
+        return true;
+    }
+}
+
+
+function mindmapdecrement() {
+    console.log("mindmapdecrement");
+    // 対象外の場合
+    if (!IsTagert()) {
+        return;
+    }
+    // アクティブなエディタを取得
+    const editor = vscode.window.activeTextEditor;
+    //選択範囲を取得
+    const ref = editor.selection;
+    // 選択された文字列を取得する
+    const selectedtxt = editor.document.getText(ref);
+
+    // 改行で分割する
+    var repstr = selectedtxt.replace("\r\n", "\n");
+    var splitStrObj = repstr.split("\n");
+
+    var newref = "";
+    for (var i = 0; i < splitStrObj.length; i++) {
+        var newtext = splitStrObj[i];
+
+        console.log("getNewStringを呼び出すよ");
+        var updtext = getNewString(newtext, false);
+
+        // 初回文字だと、置き換えで、2件目以降は追加してく。
+        if (newref.length == 0) {
+            newref = updtext;
+        } else {
+            newref = newref + "\n" + updtext;
+        }
+
+
+    }
+    // editorに反映させる
+    editor.edit(builder => {
+        builder.replace(ref, newref)
+    })
+
+    return [];
+
+}
+
+function mindmapincrement() {
+    console.log("mindmapincrement");
+    // 対象外の場合
+    if (!IsTagert()) {
+        return;
+    }
+
+    // アクティブなエディタを取得
+    const editor = vscode.window.activeTextEditor;
+
+    //選択範囲を取得
+    const ref = editor.selection;
+
+
+    // 選択された文字列を取得する
+    const selectedtxt = editor.document.getText(ref);
+
+    // 改行を統一し、分解
+    var repstr = selectedtxt.replace("\r\n", "\n");
+    var splitStrObj = repstr.split("\n");
+
+    var newref = "";
+    // 選択行分繰り返し
+    for (var i = 0; i < splitStrObj.length; i++) {
+        var newtext = splitStrObj[i];
+
+        console.log("getNewStringを呼び出すよ");
+        var updtext = getNewString(newtext, true);
+
+        // 初回文字だと、置き換えで、2件目以降は追加してく。
+        if (newref.length == 0) {
+            newref = updtext;
+        } else {
+            newref = newref + "\n" + updtext;
+        }
+
+
+    }
+    // editorに反映させる
+    editor.edit(builder => {
+        builder.replace(ref, newref)
+    })
+
+    return [];
+
+}
+
+
+/**
+ * @param {string} newtext
+ */
+function getNewString(newtext, goRight) {
+    console.log("getNewString-start!");
+    console.log(newtext);
+
+    var selectedtxt = newtext;
+
+    // 1文字目を取り出す
+    var srcword = newtext.substring(0, 1);
+
+    var IsKeyWord = false;
+
+    const regex = RegExp(`[\\+|\\-|\*|']`);
+    console.log("今回の選択文字：", srcword);
+    // 予約語ではじまっているか？
+    IsKeyWord = regex.test(srcword);
+
+    // todo:1文字目から分のはじまりまでを取得する。。
+
+    // 1-1-1.開始文字が plantumlで使う予約語？（[+|-|*|']）の場合
+    if (IsKeyWord) {
+        console.log('予約語でした.');
+
+        // チェック用文字列の準備
+        const regexplus = RegExp("\\+");
+        const regminus = RegExp("\\-");
+        var IsPlusstr = false;
+
+
+        IsPlusstr = regexplus.test(srcword);
+
+        var IsMinusstr = false;
+        IsMinusstr = regminus.test(srcword);
+
+        if (IsPlusstr) {
+            // 1-1-1-a.+ の場合		
+
+            // + 文字であればスペースなしで+だけを増やす
+            console.log('予約語＋でした.');
+            // - 開始文字の前に、[+]を追加する
+
+            // 右に行く場合
+            if (goRight) {
+                newtext = "+" + selectedtxt;
+            } else {
+                // - を1つ消す。
+                var endpos = selectedtxt.length;
+                newtext = selectedtxt.substring(1, endpos);
+
+            }
+
+
+        } else if (IsMinusstr) {
+            console.log('予約語－でした.');
+
+            // 右に行く場合
+            if (goRight) {
+                // - マイナスを1つ消す。
+                var endpos = selectedtxt.length;
+                newtext = selectedtxt.substring(1, endpos);
+            } else {
+                newtext = "-" + selectedtxt;
+            }
+            // 1-1-1-b.- の場合
+
+
+        } else {
+            console.log('予約語＠その他でした.');
+            // 1-1-1-c.* の場青
+            // - なにもしない
+        }
+
+    } else {
+        // 1-1-2.上記以外の場合
+        console.log('予約語以外でした.');
+
+
+        // 右に行く場合
+        if (goRight) {
+            newtext = "+ " + selectedtxt;
+        } else {
+            newtext = "- " + selectedtxt;
+        }
+    }
+    return newtext;
+
+}
 
 // this method is called when your extension is deactivated
 function deactivate() {}
